aae33dae24dddf77bcac74f6c5334afe8e9c7615
Uyen Tran <kiity2509@gmail.com>
2021-03-17 02:47:43 -0700

    change Vigenere code
    Seperate keyfile and encrypted file

--- VigenereCipher.cpp
+++ VigenereCipher.cpp
@@ -17,6 +17,7 @@ void VigenereMenu(){
     //use app to append and not overwrite when start again
     ofstream newfile("encryptFile.txt", fstream::app);
     ofstream DecryptFile("decryptFile.txt", fstream::app);
+    ofstream keyFile("keyFile.txt", fstream::app);
 
     //main menu
     cout << "Hello, Welcome to Vigenere Cipher \n";
@@ -42,12 +43,12 @@ void VigenereMenu(){
         cout << "Do you want to print the messages to txt file? (y/Y) or (n/N)\n";
         cin >> answer;
         if(answer == 'y' || answer == 'Y'){
-                newfile << "key for the file: " << newKey << '\n';
-                newfile << "encrypt message: " << encryptMessages << endl;
+            keyFile << "key for the file: " << newKey << '\n';
+            newfile << encryptMessages << endl;
 		}
         if(answer == 'n' || answer == 'N'){
             cout <<"key for the decrypt: " << newKey << endl;
-            cout << encryptMessages << endl;
+            cout << "Encrypted messages " << encryptMessages << endl;
 		}
 
         //ask user if they want to continue or not
@@ -70,7 +71,7 @@ void VigenereMenu(){
         cout << "Do you want to print the messages to txt file? (y/Y) or (n/N)\n";
         cin >> answer;
         if(answer == 'y' || answer == 'Y'){
-                DecryptFile << "Decrypt message: " << decryptMess << '\n';
+                DecryptFile << decryptMess << '\n';
 		}
         if(answer == 'n' || answer == 'N'){
             DecryptFile << "Decrypt message: " << decryptMess << '\n';
