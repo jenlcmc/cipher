9763426d4b2cd1fbf0c1b4029569c02e8fb09aa4
Chris <padillachris@protonmail.com>
2021-05-12 02:49:47 -0700

    modified:   README.md 	modified:   caesar.cpp

--- README.md
+++ README.md
@@ -14,7 +14,17 @@ From Terminal(Unix/Linux):
 Should produce a main file if not already in folder:
 
     ./main
-    
+
+## Remove VSCode folders
+
+.vscode folders are used for running and debugging within the VS Code text editor
+
+.vscode folder may be hidden in Unix like systems but visible on Windows OS to find remove from terminal/powershell:
+
+    ls -la //.vscode folder should be visible
+   
+    rm -rf .vscode
+
 ## Known Issues
 
 _issues with using {} initialization on OSX compilers that does not use GDB_
--- caesar.cpp
+++ caesar.cpp
@@ -1,11 +1,19 @@
 #include "caesar.h"
 
+
+/* Run creaser program */
 void caesar::run_program(){
     int choice = 0, shift = 0;
-    std::string message, p1, p2;
+    std::string message = "", p1 ="", p2 ="";
 
-    std::cout << "\tEnter message followed by:\n\n\t(1)Encode or (2)Decode\n\n"
-            << "\tShift/key value(1-26)\n"; 
+    // separator for display
+    for(auto i = 0; i < 10; ++i){
+        std::cout << "*";
+    } std::cout << std::endl;
+    std::cout << "Enter message followed by:\n(1)Encode or (2)Decode\n"
+            << "Shift/key value(1-26)\n"; 
+
+    /* get user input message and key value*/
     std::getline(std::cin, message);
     std::cin >> choice >> shift;
  
@@ -13,8 +21,8 @@ void caesar::run_program(){
     std::cout <<message << std::endl; //print message
 
     if(choice == 1) {        
-        p1 = caesar::encrypt(message, shift);
-        p2 = caesar::decrypt(p1, shift);
+        p1 = caesar::encrypt(message, shift); //store into p1
+        // p2 = caesar::decrypt(p1, shift); //store into p2; commented out, was being used for testing 
         std::cout << "Encode: " << p1 << std::endl;
         caesar::create_file(p1,shift); std::cout << std::endl; //flushes buffer when creating a file
 
@@ -25,29 +33,34 @@ void caesar::run_program(){
     }
 }
 
+/* fuction takes a string for and int for message and shift values */
 std::string caesar::encrypt(std::string& message, int shift){
     std::string vals = ""; 
     for(auto i = 0; i < message.length(); ++i){
         if(isupper(message[i])){
             vals += char(int(message[i] + shift - 65)%26 + 65); //converts integers to char values
         } else {
-            vals += char(int(message[i] + shift - 97)%26 + 97);  //and addes them into the string
+            vals += char(int(message[i] + shift - 97)%26 + 97);  //and adds them into the string
         }
     }
     return vals;
 }
-
+/* this function calls the encrypt function and substracts the amount from the alphabet and the shift/key value */
 std::string caesar::decrypt(std::string& message, int shift){
     shift%=26;
     return caesar::encrypt(message, 26 - shift);
 }
 
+/* using the algorithm library for the erase and remove function.
+    remove iterates throuhg the message with begin and end (where to start, where to end, and what to search for)
+    erase when it hits the end
+ */
 std::string caesar::remove_space(std::string& message){
     message.erase(std::remove(message.begin(), message.end(), ' '), message.end());
     return message;
 }
 
-//write into a file
+//write into a file using ostream library
 int caesar::create_file(std::string& message,int shift){
     std::ofstream opf {"./Text_Files/binary.txt"};
 
